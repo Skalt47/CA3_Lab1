d39821ead962703bfa71412b564c8addd008e004
Tim Jauch <tijait00@hs-esslingen.de>
2025-04-25 23:52:10 +0200

    Implement LED counter for Preparation Task 1.1

--- lab1a/Sources/main.asm
+++ lab1a/Sources/main.asm
@@ -21,40 +21,81 @@
         INCLUDE 'mc9s12dp256.inc'
 
 ; Defines
-;SPEED:  EQU     $FFFF                   ; Change this number to change counting speed
- SPEED_X: EQU    100
- SPEED_Y: EQU    65535
+SPEED:  EQU     $FFFF                   ; Change this number to change counting speed
+SPEED_X: EQU    100
+SPEED_Y: EQU    65535
+
 
 ; RAM: Variable data section
 .data: SECTION
+counter_L: DS.B 1          ; Low byte of counter (8 bits)
+counter_H: DS.B 1          ; High byte of counter (8 bits)
 
 ; ROM: Constant data
 .const: SECTION
 
+
 ; ROM: Code section
 .init: SECTION
+
 
 main:                                   ; Begin of the program
 Entry:
         LDS  #__SEG_END_SSTACK          ; Initialize stack pointer
         CLI                             ; Enable interrupts, needed for debugger
-        
-        LDAA #$FF
-        STAA DDRB
 
-; --- ADDED FROM HERE ---
+         LDAA    #$FF
+        STAA    DDRB                    ; Configure PORTB as output
+
+        ; --- Initialize counter to 0 ---
+        CLRA
+        STAA    counter_L
+        STAA    counter_H
+
 loop:
-        LDAA    #$AA                    ; Load pattern (10101010) into A
-        STAA    PORTB                   ; Output to LEDs
-        JSR     delay                   ; Wait
-        LDAA    #$00                    ; Turn off LEDs
-        STAA    PORTB
-        JSR     delay
-        BRA     loop                    ; Repeat forever
-; --- ADDED UNTIL HERE ---
+        ; --- Load 16-bit counter into X register ---
+        LDAA    counter_L
+        LDAB    counter_H
+        PSHB
+        PSHA
+        PULX                            ; A = low, B = high ? combined to X
 
-; --- ADDED: simple delay subroutine ---
-delay:
+        ; Optional: store back (not necessary here)
+        STX     counter_L
+
+        ; --- Output low byte to LEDs ---
+        LDAA    counter_L
+        STAA    PORTB
+
+        ; --- Delay (~0.5s) ---
+        JSR     delay_0_5sec
+
+        ; --- Increment counter by 2 ---
+        LDAA    counter_L
+        ADDA    #2
+        STAA    counter_L
+
+        ; --- Carry to high byte if needed ---
+        BCC     no_carry
+        INC     counter_H
+no_carry:
+
+        ; --- Reset if counter reaches 64 ---
+        LDAA    counter_L
+        CMPA    #64
+        BNE     loop
+
+        CLRA
+        STAA    counter_L
+        STAA    counter_H
+        BRA     loop
+
+; ---------------------------------------------------------
+; Subroutine: delay_0_5sec
+; Delay ~0.5 seconds (nested loop)
+; Registers D, X, Y may be modified
+; ---------------------------------------------------------
+delay_0_5sec:
         LDX     #SPEED_X
 outer_loop:
         LDY     #SPEED_Y
@@ -64,6 +105,3 @@ inner_loop:
         DEX
         BNE     outer_loop
         RTS
-; --- END OF ADDED SECTION --- 
-
-
